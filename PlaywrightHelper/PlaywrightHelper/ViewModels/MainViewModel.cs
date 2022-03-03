5e94eb43241538caca1dc0f9961b96c9fb38ca1e
Ron Trolard <ron@digitalconcrete.net>
2022-03-03 11:08:05 -0600

    Initial commit

---
+++ PlaywrightHelper/PlaywrightHelper/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaywrightHelper.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ PlaywrightHelper/PlaywrightHelper/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PlaywrightHelper.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
