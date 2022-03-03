68b6fd568d30a4ff34b064afc24d867f21165a81
Ron Trolard <ron@digitalconcrete.net>
2022-03-03 17:23:27 -0600

    Next round of updates... button starting playwright, next step is either recording through the command line or something else

--- PlaywrightHelper/PlaywrightHelper/ViewModels/MainViewModel.cs
+++ PlaywrightHelper/PlaywrightHelper/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
+using Microsoft.Playwright;
 using PlaywrightHelper.Views;
 
 namespace PlaywrightHelper.ViewModels
@@ -10,13 +11,32 @@ namespace PlaywrightHelper.ViewModels
     public partial class MainViewModel : ViewModelBase
     {
         public ObservableCollection<FileDisplay> ScriptSource { get; }
+        private string rootPath;
+        public async void RecordingStarted()
+        {
+            var exitCode = Microsoft.Playwright.Program.Main(new[] {"install"});
+            if (exitCode != 0)
+            {
+                throw new Exception($"Playwright exited with code {exitCode}");
+            }            
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
+            {
+                Headless = false,
+                TracesDir = rootPath
+            });
+            var context = await browser.NewContextAsync(); // Pass any options
+            var page = await browser.NewPageAsync();
+            await page.PauseAsync();
+
+        }
 
         public MainViewModel()
         {
             var home = Environment.GetEnvironmentVariable("HOME");
             if(string.IsNullOrEmpty(home))
                 home = Environment.GetEnvironmentVariable("USER_PROFILE");
-            var rootPath = $"{home}{Path.DirectorySeparatorChar}playwrightScripts";
+            rootPath = $"{home}{Path.DirectorySeparatorChar}playwrightScripts";
             if (!Directory.Exists(rootPath))
                 Directory.CreateDirectory(rootPath);
             var pathInfo = new DirectoryInfo(rootPath);
--- PlaywrightHelper/PlaywrightHelper/Views/MainView.axaml.cs
+++ PlaywrightHelper/PlaywrightHelper/Views/MainView.axaml.cs
@@ -1,8 +1,4 @@
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
 using Avalonia.Controls;
-using Avalonia.Metadata;
 
 namespace PlaywrightHelper.Views
 {
